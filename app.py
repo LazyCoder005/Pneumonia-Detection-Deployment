967c95e46661a52765988346a002e4eb9173546b
LazyCoder005 <85675517+LazyCoder005@users.noreply.github.com>
2021-09-20 10:57:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,6 +82,12 @@ def upload():
 
 
 if __name__ == '__main__':
+    app.run(debug=True)
+
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
+#     app.run(debug=True)
+
     
     http_server = WSGIServer(('', 5000), app)
     http_server.serve_forever()
